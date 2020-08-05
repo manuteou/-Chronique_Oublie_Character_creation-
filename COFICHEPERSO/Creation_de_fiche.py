707ad90bc818d10b870224ba4f3567b4a70f185a
manu <frogenmanu@hotmail.com>
2020-08-05 17:15:38 +0200

    premiere sauvegarde

---
+++ COFICHEPERSO/Creation_de_fiche.py
@@ -0,0 +1,77 @@
+import random
+from COFICHEPERSO import Personnages
+from COFICHEPERSO import Caracteristiques
+
+roll = [random.randint(3,18) for r in range(6)] # Dés lancées pour la valeurs des caractériqtiques du personnage
+
+def attribution(valeur_choisie):
+    """fonction qui permet d'attirbuer les valeurs du lancé de dé et d'effacer la valeur"""
+    fin = True
+    if valeur_choisie in roll:
+            roll.remove(valeur_choisie)
+            print("valeur attribuée")
+            return valeur_choisie
+
+    else:
+        while fin:
+            print(f"la valeur choisie doit etre parmis {roll}")
+            valeur_choisie = int(input("Rentrez une valeur valide"))
+            if valeur_choisie in roll:
+                roll.remove(valeur_choisie)
+                fin = False
+        return valeur_choisie
+
+def profile_choix():
+    """ fonction qui definie les profiles les bonus de profiles et equipements de profile"""
+    pass
+
+def race_choix():
+    """ fonction qui definie la race est les bonus de races"""
+    pass
+
+def age_choix():
+    """ foncition qui donne un age est correcte pour la race"""
+    pass
+
+def taille_choix():
+    """fonction qui donne une taille correcte pour la race"""
+    pass
+
+def poids_choix():
+    """ fonction qui donne un poids correcte pour la race"""
+    pass
+
+print ("BIENVENUE SUR LE GENERATEUR DE PERSONNAGE CHRONIQUE OUBLIEE")
+
+Fichier_joueur = {}
+Joueur_Perso = Personnages.Personnages()
+
+nom = input("Bonjour aventurier, quel est ton prénom :")
+nom_hero = input("Quel nom veux tu donner à ton personnage :")
+profile = input("Quel est ton profile :")
+race = input("Quel est ta race :")
+sexe = input("Quel est ton genre :")
+age = input("Quel est ton age :")
+taille = input("Quel est ta taille :")
+poids = input("Quel est ton poids")
+
+Joueur_Perso.set_nom_joueur(nom)
+Joueur_Perso.set_nom_personnage(nom_hero)
+Joueur_Perso.set_profile(profile)
+Joueur_Perso.set_race(race)
+Joueur_Perso.set_sexe(sexe)
+Joueur_Perso.set_age(age)
+Joueur_Perso.set_taille(taille)
+Joueur_Perso.set_poids()
+
+Fichier_joueur[Joueur_Perso.get_nom_joueur] = Joueur_Perso
+print(f"Le personnage {Joueur_Perso.get_nom_personnage() }, jouer par {Joueur_Perso.get_nom_joueur()} /"
+      f"va tirer ses attributs")
+
+
+print (f"Valeurs du lancé de dé : {roll}")
+Joueur_Carac = Caracteristiques.Caractéristiques()
+force = int(input("Quelle valeur pour la caracteristique force :"))
+Joueur_Carac.set_force(attribution(force))
+
+
---
+++ COFICHEPERSO/Personnages.py
@@ -0,0 +1,71 @@
+class Personnages:
+    """" Class qui definie le type  du personnage et son aspect physique"""
+    def __init__(self,nom_personnage,nom_joueur,profile,niveau,race,sexe,age,taille,poids):
+        self.nom_personnage = nom_personnage
+        self.nom_joueur = nom_joueur
+        self.profile = profile
+        self.niveau = niveau
+        self.race = race
+        self.sexe = sexe
+        self.age = age
+        self.taille = taille
+        self.poids = poids
+
+    def get_nom_personnage(self):
+        return self.nom_personnage
+
+    def set_nom_personnage(self, nom_personnage):
+        self.nom_personnage = nom_personnage
+
+    def get_nom_joueur(self):
+        return self._nom_joueur
+
+    def set_nom_joueur(self, nom_joueur):
+        self.nom_joueur = nom_joueur
+
+    def get_profile(self):
+        return self.profile
+
+    def set_profile(self,profile):
+        self.profile = profile
+
+    def get_niveau(self):
+        return self.niveau
+
+    def set_niveau(self,niveau):
+        self.niveau = niveau
+
+    def _get_race(self):
+        return self.race
+
+    def set_race(self,race):
+        self.race = race
+
+    def get_sexe(self):
+        return self.sexe
+
+    def set_sexe(self,sexe):
+        self.sexe = sexe
+
+    def get_age(self):
+        return self.profile
+
+    def set_age(self,age):
+        self.age = age
+
+    def get_taille(self):
+        return self.taille
+
+    def set_taille(self,taille):
+        self.taille = taille
+
+    def _get_poids(self):
+        return self.poids
+
+    def set_poids(self,poids):
+        self.poids = poids
+
+
+
+
+
