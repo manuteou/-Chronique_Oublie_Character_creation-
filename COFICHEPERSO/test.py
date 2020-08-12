716bed484a2438b0bcd9af2705179d7229788432
manuteou <frogenmanu@hotmail.com>
2020-08-12 04:34:08 +0200

    argg

--- COFICHEPERSO/test.py
+++ COFICHEPERSO/test.py
@@ -1,22 +1,15 @@
 from tkinter import *
+from COFICHEPERSO.Interface_Graphique import Interface_graphique_perso
 
-class elfe():
-    def __init__(self,character,attribut):
+class Character():
+    def __init__(self,character, attribut, description):
         self.character = character
         self.attribut = attribut
-class grap():
-    def __init__(self,main):
+        self.description = description
 
-        self.main = main
-        self.main.geometry("800x600")
-        self.value = StringVar()
-        self.value_catch = Entry(self.main, textvariable=self.value)
-        self.value_catch.pack()
-        self.Btn = Button(self.main, text="sortie", command=quit)
-        self.Btn.pack()
 
-main = Tk()
-Ig = grap(main)
+windows = Tk()
+Interface_graphique_perso(windows)
 main.mainloop()
 test = Ig.value_catch.get()
 character = {"nom_Joueur": Ig.value_catch.get(), "nom_hero": "bibi"}
