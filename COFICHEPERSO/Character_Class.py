baabdd823565531f83c4c61e929cd41c14b53db7
manuteou <frogenmanu@hotmail.com>
2020-08-15 12:46:08 +0200

    Finish

--- COFICHEPERSO/Character_Class.py
+++ COFICHEPERSO/Character_Class.py
@@ -1,6 +1,13 @@
 class Character():
 
-    attribut = {"for": 0, "dex": 0, "con": 0, "int": 0, "cha": 0, "sag": 0}
+    attribut = {
+        "for": 0, "dex": 0, "con": 0,
+        "int": 0, "cha": 0, "sag": 0
+    }
+    attribut_modification = {
+        "fom": 0, "dem": 0, "com": 0,
+        "itm": 0, "chm": 0, "sam": 0
+    }
     age = 0
     size = 0
     weight = 0
@@ -10,4 +17,5 @@ class Character():
     profile_name = ""
     life_dice = ""
     level = 1
+    racial_capacity = ""
 
--- COFICHEPERSO/Main_program.py
+++ COFICHEPERSO/Main_program.py
@@ -14,19 +14,56 @@ def pdf_genrator(pdf):
     pdf.drawImage(image, 0, 0)
     pdf.drawString(400, 668, str(player.player_name), )
     pdf.drawString(145, 668, str(player.character_name))
+    pdf.drawString(50, 623, str(player.profile_name))
 
     pdf.drawString(220, 623, str(player.race))
-    pdf.drawString(343, 623, player.gender)
-    pdf.drawString(420, 623, str(player.size) + "cm")
-    pdf.drawString(391, 623, str(player.age) + "ans")
+    pdf.drawString(342, 623, player.gender)
+    pdf.drawString(430, 623, str(player.size) + "cm")
+    pdf.drawString(388, 623, str(player.age) + "ans")
     pdf.drawString(470, 623, str(player.weight) + "Kg")
 
     pdf.drawString(80, 560, str(player.attribut["for"]))
     pdf.drawString(80, 538, str(player.attribut["dex"]))
     pdf.drawString(80, 515, str(player.attribut["con"]))
     pdf.drawString(80, 492, str(player.attribut["int"]))
-    pdf.drawString(80, 470, str(player.attribut["cha"]))
-    pdf.drawString(80, 450, str(player.attribut["sag"]))
+    pdf.drawString(80, 470, str(player.attribut["sag"]))
+    pdf.drawString(80, 450, str(player.attribut["cha"]))
+
+    pdf.drawString(112, 560, str(player.attribut_modification["fom"]))
+    pdf.drawString(112, 538, str(player.attribut_modification["dem"]))
+    pdf.drawString(112, 515, str(player.attribut_modification["com"]))
+    pdf.drawString(112, 492, str(player.attribut_modification["itm"]))
+    pdf.drawString(112, 470, str(player.attribut_modification["sam"]))
+    pdf.drawString(112, 450, str(player.attribut_modification["chm"]))
+
+    pdf.drawString(427, 560, str(player.life_dice))
+    pdf.drawString(175, 623, str(player.level))
+    pdf.drawString(310, 538, str(player.level))
+    pdf.drawString(310, 515, str(player.level))
+    pdf.drawString(310, 492, str(player.level))
+
+    pdf.drawString(270, 560, str(player.attribut_modification["dem"]))
+    pdf.drawString(270, 538, str(player.attribut_modification["fom"]))
+    pdf.drawString(270, 515, str(player.attribut_modification["dem"]))
+
+    pdf.drawString(40, 400, str(player.racial_capacity))
+
+
+def attribut_modification(player_attribut):
+    """fonction who calcul the player bonus attribut"""
+    table = {
+        1: -4, 2: -4, 3: -4,
+        4: -3, 5: -3, 6: -2,
+        7: -2, 8: -1, 9: -1,
+        10: 0, 11: 0, 12: 1,
+        13: 1, 14: 2, 15: 2,
+        16: 3, 17: 3, 18: 4,
+        19: 4, 20: 5, 21: 5
+    }
+
+    for table_attribut_key, modification_value in table.items():
+        if table_attribut_key == player_attribut:
+            return modification_value
 
 
 # loading Characters_race
@@ -69,15 +106,26 @@ player.player_name = GUI_player_selection.player_name_entry.get()
 player.character_name = GUI_player_selection.hero_name_entry.get()
 player.race = race_user_selection
 player.profile_name = profile_user_selection
+player.life_dice = profile_selection["life_dice"]
+player.racial_capacity = race_selection["capacite raciale"]
+
 player.age = randint(race_selection["age"]["min"], race_selection["age"]["max"])
 player.weight = randint(race_selection["taille"]["min"], race_selection["taille"]["max"])
 player.size = randint(race_selection["taille"]["min"], race_selection["taille"]["max"])
+
 player.attribut["for"] = int(GUI_player_selection.Str_Value.get()) + race_selection["modif"]["for"]
 player.attribut["dex"] = int(GUI_player_selection.Dex_Value.get()) + race_selection["modif"]["dex"]
 player.attribut["con"] = int(GUI_player_selection.Con_Value.get()) + race_selection["modif"]["con"]
 player.attribut["int"] = int(GUI_player_selection.Dex_Value.get()) + race_selection["modif"]["int"]
-player.attribut["cha"] = int(GUI_player_selection.Con_Value.get()) + race_selection["modif"]["cha"]
 player.attribut["sag"] = int(GUI_player_selection.Sag_Value.get()) + race_selection["modif"]["sag"]
+player.attribut["cha"] = int(GUI_player_selection.Con_Value.get()) + race_selection["modif"]["cha"]
+player.attribut_modification["fom"] = attribut_modification(player.attribut["for"])
+player.attribut_modification["dem"] = attribut_modification(player.attribut["dex"])
+player.attribut_modification["com"] = attribut_modification(player.attribut["con"])
+player.attribut_modification["itm"] = attribut_modification(player.attribut["int"])
+player.attribut_modification["sam"] = attribut_modification(player.attribut["sag"])
+player.attribut_modification["cam"] = attribut_modification(player.attribut["cha"])
+
 if GUI_player_selection.get_gender_value.get() == 4:
     player.gender = "Homme"
 elif GUI_player_selection.get_gender_value.get() == 7:
@@ -91,3 +139,6 @@ pdf = canvas.Canvas(name_pdf)
 pdf_genrator(pdf)
 pdf.showPage()
 pdf.save()
+
+print(player.attribut_modification)
+print(player.attribut)
